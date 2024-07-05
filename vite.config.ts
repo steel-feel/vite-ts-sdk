a45429c525598e003175203978be16bfc5a52a67
himank jain <himankjain@yahoo.in>
2024-07-06 03:28:48 +0530

    formating

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-    build: {
+  build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'MyLib',
